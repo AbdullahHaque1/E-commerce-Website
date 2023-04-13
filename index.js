71771779f95d78d60050929630d39445d06ddac9
abhaque333@gmail.com <100842119+AbdullahHaque1@users.noreply.github.com>
2023-04-13 23:20:10 +0530

    First project

---
+++ index.js
@@ -0,0 +1,11 @@
+var navLinks = document.querySelector("#nav_Links");
+
+
+function showMenu()
+{
+    navLinks.style.visibility = "visible";
+}
+function hideMenu()
+{
+    navLinks.style.visibility = "hidden";
+}
